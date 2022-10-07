5c1c5b8e24b3c5e6f1933c4cf6f31cba9b3797f3
vitoriakarpovas <64480334+vitoriakarpovas@users.noreply.github.com>
2022-10-07 16:11:31 -0300

    Update primos.c

--- lab05/primos.c
+++ lab05/primos.c
@@ -1,29 +1,79 @@
-#include <stdio.h>
+#include "mpi.h"
 #include <math.h>
-#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-#define LIMITE 5000000
+#define MAXSIZE 5000
 
-
-int primo(int numero){
-    int raiz, fator;
-    raiz = (int) sqrt((double) numero);
-    for(fator = 2; fator <= raiz; fator++)  
-        if (numero % fator == 0)    
-            return 0;
-        return 1;
+int isPrime(int numero)
+{
+  int raiz, fator;
+  raiz = (int)sqrt((double)numero);
+  for (fator = 2; fator <= raiz; fator++)
+    if (numero % fator == 0)
+    {
+      return 0;
+    }
+  return 1;
 }
 
-int main(){
-    int quantidade = 0, numero;
-    double dif;    // tomada de tempo
-    
-    
-    for(numero = 2; numero < LIMITE; numero ++) {  
-        int p = primo(numero);
-        quantidade += p;
-    } 
-    
-    printf("Total de numeros primos ate %d: %d\n", LIMITE, quantidade);
-    
+int main(int argc, char **argv)
+{
+  int myid, numprocs;
+  int myTotalPrimes = 0, totalPrimes = 0;
+  int *primes;
+  int *myPrimes;
+  int i, range, low, high;
+  int j, k;
+  myPrimes = (int *)malloc(sizeof(int) * 1);
+  primes = (int *)malloc(sizeof(int) * MAXSIZE);
+
+  j = strtol(argv[1], NULL, 10);
+  k = strtol(argv[2], NULL, 10);
+
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+  range = round((k - j) / numprocs);
+  low = ((myid * range) + j) + myid;
+  high = low + range;
+  if (high > k)
+    high = k;
+
+  for (i = low + 1; i <= high; i++)
+  {
+    if (isPrime(i))
+    {
+      myPrimes[myTotalPrimes] = i;
+      myTotalPrimes++;
+      myPrimes = (int *)realloc(myPrimes, (myTotalPrimes) * sizeof(int));
+    }
+  }
+
+  char hostname[30];
+  gethostname(hostname, 30);
+
+  printf("I got %d from %d - %s\n", myTotalPrimes, myid, hostname);
+
+  MPI_Reduce(&myTotalPrimes, &totalPrimes, 1, MPI_INT, MPI_SUM, 0,
+             MPI_COMM_WORLD);
+
+  MPI_Gather(myPrimes, myTotalPrimes, MPI_INT, primes, myTotalPrimes, MPI_INT,
+             0, MPI_COMM_WORLD);
+
+  if (myid == 0)
+  {
+    printf("The total primes is %d.\n", totalPrimes);
+
+    for (int i = 0; i < totalPrimes + 1; i++)
+    {
+      printf("%d\n", primes[i]);
+    }
+  }
+
+  MPI_Finalize();
+  return 0;
 }
