dfaed88f2f1ed98c0faebb190ad691ae154bc237
vitoriakarpovas <64480334+vitoriakarpovas@users.noreply.github.com>
2022-10-11 12:55:33 -0300

    Update primoAtualizado.c

--- lab05/primoAtualizado.c
+++ lab05/primoAtualizado.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv){
   int myTotalPrimes = 0, totalPrimes = 0;
   int *primes;
   int *myPrimes;
-  int i, range, low, high;
+  int i, low, high;
   int j, k;
   double time;
 
@@ -37,37 +37,24 @@ int main(int argc, char **argv){
 
   low = ((numprocs - myid) *low + (myid) *(MAXSIZE + 1))/(numprocs);
   high = ((numprocs - myid - 1)*low + (myid + 1) * (MAXSIZE + 1))/(numprocs) - 1;
-  if (high > k)
-    high = k;
 
   for (i = low + 1; i <= high; i++)
   {
     if (isPrime(i))
     {
-      myPrimes[myTotalPrimes] = i;
       myTotalPrimes++;
-      myPrimes = (int *)realloc(myPrimes, (myTotalPrimes) * sizeof(int));
     }
   }
-
-  char hostname[500];
-  gethostname(hostname, 500);
 
   printf("I got %d from %d\n", myTotalPrimes, myid);
 
   MPI_Reduce(&myTotalPrimes, &totalPrimes, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 
-  MPI_Gather(myPrimes, myTotalPrimes, MPI_INT, primes, myTotalPrimes, MPI_INT, 0, MPI_COMM_WORLD);
-
   if (myid == 0)
   {
     time = MPI_Wtime() - time;
     printf("The total primes is %d.\n", totalPrimes);
-
-    for (int i = 0; i < totalPrimes + 1; i++)
-    {
-      printf("%d\n", primes[i]);
-    }
+    printf("\ntime: %14f", time);
   }
 
   MPI_Finalize();
